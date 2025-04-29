bb64505f7f2f8a41d13ade256a623050597ed54d
Aleksandra <tamonovaas@gmail.com>
2025-04-29 16:25:45 +0300

    add superadmin. New views. New model book with one atribut 'Title'

---
+++ books/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+class Book(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+        ordering = ['title']
