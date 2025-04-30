3dd79f6f3ca8e0504843ff088ac0f105a18a3094
Aleksandra <tamonovaas@gmail.com>
2025-04-30 20:19:12 +0300

    Books list and special form for this

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from books.models import Book
+
+__all__ = (
+    'home',
+)
+def home(request):
+    qs = Book.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'books/home.html', context)
